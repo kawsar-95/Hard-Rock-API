b45be5b68306088e154dd6563711a07e551009db
kawsar-95 <virtualkawsar@gmail.com>
2020-08-17 23:49:56 +0600

    done

--- script.js
+++ script.js
@@ -1,40 +1,106 @@
-//load data and key
-const musicLyricsApp = {
-  key: "2c1ebdc953641626c209cf060c2fe39f",
-  baseUrl: "https://api.lyrics.ovh/suggest/kawsar1",
+let singleLyrics = document.querySelector(".single-lyrics");
+let fancySearchResult = document.querySelector(".search-result");
+let simpleSearchResult = document.querySelector(".simple-result");
+
+// Displays the lyrics on the page
+const displayLyrics = (song, artist, title) => {
+  // alert
+  if (!song.lyrics) {
+    alert(" Lyrics not found.");
+    return;
+  }
+
+  singleLyrics.innerHTML = `
+        <button class="btn go-back" onclick = "showSongsResults()">& lsaquo;</button>
+        <h2 class="text-success mb-4">${title} - ${artist} </h2>
+        <pre class="lyric text-white">${song.lyrics}</pre>
+    `;
+
+  simpleSearchResult.style.display = "none";
+  fancySearchResult.style.display = "none";
+  singleLyrics.style.display = "block";
 };
 
-//fetch Lyrics API
-function lyricsAPI(song) {
-  fetch(
-    `${musicLyricsApp.baseUrl}?q=${song}&appId=${musicLyricsApp.key}&units=metric`
-  )
-    .then((lyricsCheck) => {
-      return lyricsCheck.json();
-    })
-    .then(lyricsShow);
-}
-//Search Button Handler
+//  lyrics of the song
+const fetchLyric = (artist, title) => {
+  fetch(`https://api.lyrics.ovh/suggest/kawsar1/${artist}/${title}`)
+    .then((response) => response.json())
+    .then((json) => displayLyrics(json, artist, title))
+    .catch((error) => alert(error));
+};
 
-document.getElementById("searchButton").addEventListener("click", () => {
-  const lyricsSearch = document.getElementById("searchSong").value;
+//   simple design
+const simpleSongResults = (songTitle, albumTitle, artistName) => {
+  const simpleSong = document.createElement("div");
+  simpleSong.className = "text-center";
+  simpleSong.innerHTML = `
+            <p class="author lead">
+                <strong>${albumTitle}</strong> Album by <span>${artistName}</span> 
+                <button onclick = "fetchLyric('${artistName}','${songTitle}')" class="btn btn-success">
+                    Get Lyrics
+                </button>
+            </p>
+    `;
 
-  lyricsAPI(lyricsSearch);
+  simpleSearchResult.appendChild(simpleSong);
+};
+
+//   fancy design
+const fancySongResults = (songTitle, albumTitle, artistName) => {
+  const songInfo = document.createElement("div");
+  songInfo.className = "single-result row align-items-center my-3 p-3";
+  songInfo.innerHTML = `
+    <div class="col-md-9" id="songInfo">
+        <h3 class="lyrics-name">${songTitle}</h3>
+        <p class="author lead">${albumTitle} Album by <span>${artistName}</span></p>
+    </div>
+    <div class="col-md-3 text-md-right text-center" id="getLyrics">
+        <button onclick = "fetchLyric('${artistName}','${songTitle}')" class="btn btn-success">Get Lyrics</button>
+    </div>`;
+
+  fancySearchResult.appendChild(songInfo);
+};
+
+// Display the songs
+const displaySongs = (song) => {
+  const songTitle = song.title;
+  const albumTitle = song.album.title;
+  const artistName = song.artist.name;
+
+  simpleSongResults(songTitle, albumTitle, artistName);
+  fancySongResults(songTitle, albumTitle, artistName);
+};
+
+// Handles api song info
+const handleSongs = (songs) => {
+  if (!songs.data) {
+    alert("Can't Fetch Any Data");
+    return;
+  }
+
+  simpleSearchResult.style.display = "block";
+  fancySearchResult.style.display = "block";
+
+  for (let [index, song] of songs.data.entries()) {
+    if (index == 10) {
+      break;
+    }
+    displaySongs(song);
+  }
+};
+
+// Fetch the results of the songs
+const allSongs = (songName) => {
+  fetch(`https://api.lyrics.ovh/suggest/${songName}`)
+    .then((response) => response.json())
+    .then((json) => handleSongs(json))
+    .catch((error) => alert(error));
+};
+
+//  search button
+document.getElementById("search-song").addEventListener("click", function () {
+  const songName = document.getElementById("song-name").value;
+
+  singleLyrics.style.display = "none";
+  allSongs(songName);
 });
-
-function lyricsShow(lyricsCheck) {
-  console.log(lyricsCheck);
-  // let setCity = document.getElementById("setCity");
-  // setCity.innerHTML = `${lyricsCheck.name} ${lyricsCheck.sys.country}`;
-  // let setDegree = document.getElementById("degree");
-  // setDegree.innerHTML = `${lyricsCheck.main.temp}`;
-  // let weatherName = document.getElementById("weatherName");
-  // weatherName.innerHTML = `${lyricsCheck.weather[0].main}`;
-  // // let min = document.getElementById("min");
-  // //    min.innerHTML=`${lyricsCheck.main.temp_min}`
-  // //    let max =  document.getElementById('max');
-  // //    max.innerText=`${lyricsCheck.main.temp_max}`
-  // let date = document.getElementById("dateManage");
-  // let get = new Date();
-  // date.innerHTML = getDateFunction(get);
-}
